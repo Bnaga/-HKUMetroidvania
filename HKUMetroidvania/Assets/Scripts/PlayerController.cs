5ba6a6fecff03d67cfcbb2e6196990964852f5fb
Robbert <Bayangannaga@hotmail.com>
2017-06-25 00:41:01 +0200

    working game + animation

---
+++ HKUMetroidvania/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : PhysicsObject {
+
+    public float maxSpeed = 7;
+    public float jumpTakeOffSpeed = 7;
+    private bool shoot;
+    bool faceLeft;
+    float moverx;
+
+    private Animator animator;
+
+
+	// Use this for initialization
+	void Awake ()
+    {
+        animator = GetComponent<Animator>();
+	}
+
+    // Update is called once per frame
+    protected override void ComputeVelocity()
+    {
+        Vector2 move = Vector2.zero;
+
+        move.x = Input.GetAxis("Horizontal");
+        moverx = Input.GetAxisRaw("Horizontal");
+
+        if (Input.GetButtonDown("Jump") && grounded)
+        {
+            velocity.y = jumpTakeOffSpeed;
+        }
+
+        else if(Input.GetButtonUp("Jump"))
+        {
+            if (velocity.y > 0)
+                velocity.y = velocity.y * 0.5f;
+        }
+        if(move.x < 0f)
+        {
+            faceLeft = true;
+        }
+        else if(move.x > 0f)
+        {
+            faceLeft = false;
+        }
+
+        animator.SetBool("FaceLeft", faceLeft);
+        animator.SetFloat("MoveX", moverx);
+        animator.SetBool("Grounded", grounded);
+        animator.SetFloat("VeloX", Mathf.Abs(velocity.x) / maxSpeed);
+        animator.SetBool("grounded", grounded);
+        animator.SetFloat("velocityX", Mathf.Abs(velocity.x) / maxSpeed);
+        animator.SetFloat("VeloY", Mathf.Abs(velocity.y) / jumpTakeOffSpeed);
+
+
+        targetVelocity = move * maxSpeed;
+    }
+
+
+}
---
+++ HKUMetroidvania/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    float walkSpeed = 10;
+    float runSpeed = 5;
+    public int jumpCount = 2;
+    private Rigidbody2D playerBody;
+    private bool playerMoving;
+    public float jumpSpeed = 20.0f;
+
+    // Use this for initialization
+    void Start ()
+    {
+        playerBody = GetComponent<Rigidbody2D>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        float move = Input.GetAxis("Horizontal");
+
+        playerBody.velocity = new Vector2(move * walkSpeed, playerBody.velocity.y);
+
+        if(jumpCount >= 1 && Input.GetButtonDown("Jump"))
+        {
+            playerBody.velocity = new Vector2(move * walkSpeed, jumpSpeed);
+            jumpCount -= 1;
+        }
+    }
+}
